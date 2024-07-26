7ca85c1dbc520a582efe0c5186d4075d23115e5c
anhtuan <anhtuan.mmt@gmail.com>
2024-07-26 11:11:42 +0700

    fix: camera layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,34 @@
-import React from "react";
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import ApexChart from "../components/Chart";
 import Card from "@mui/material/Card";
 import CardHeader from '@mui/material/CardHeader';
-import CardOverflow from '@mui/joy/CardOverflow';
-import Box from '@mui/material/Box';
+import CardContent from '@mui/material/CardContent';
 import Gauge from "../components/Gauge";
 import RadialGauge from "../components/RadialGauge";
-import CardContent from '@mui/material/CardContent';
-import ReactPlayer from 'react-player'
 
 function Dashboard() {
   const [chart, setChart] = useState();
   const [gauge, setGauge] = useState();
+  const [count, setCount] = useState();
+  
   const fetchData = async () => {
     const chartResponse = await fetch("http://localhost:8080/sensors/dashboard/chart");
-    console.log(chartResponse);
     chartResponse.json().then((data) => {
       setChart(data);
     });
 
     const gaugeResponse = await fetch("http://localhost:8080/sensors/dashboard/gauge");
-    console.log(gaugeResponse);
     gaugeResponse.json().then((data) => {
       setGauge(data);
     });
+
+    const countResponse = await fetch("http://localhost:8080/sensors/dashboard/count_motorcycle");
+    countResponse.json().then((data) => {
+      setCount(data);
+    });
+
   };
+
   useEffect(() => {
     const timer = setInterval(() => {
       fetchData();
@@ -33,45 +36,41 @@ function Dashboard() {
     return () => {
       clearInterval(timer);
     };
-  }, [chart, gauge]);
-
-  const settings = {
-    width: 300,
-    height: 200,
-  };
-
+  }, [chart, gauge, count]);
 
   return (
     <div className="container">
       <div id="left" className="child-container">
         <div id="chart1" className="chart-containers">
-          <ApexChart data={chart} title="Temperature"></ApexChart>
+          <ApexChart data={chart} title="Temperature" />
         </div>
         <div id="chart2" className="chart-containers">
-          <ApexChart data={chart} title="Humidity"></ApexChart>
+          <ApexChart data={chart} title="Humidity" />
         </div>
       </div>
       <div id="right" className="child-container">
         <div id="row-1">
-          <Card className="cv-gauge-temp">
-            <CardHeader
-              title="Temperature"
-              subheader={gauge?.timestamp}
-            />
-            <Gauge data={gauge} />
-          </Card>
-          <Card className="cv-gauge-humidity" >
-            <CardHeader
-              title="Humidity"
-              subheader={gauge?.timestamp}
-            />
-            <RadialGauge data={gauge} />
-          </Card>
+          <div id="gauge-left">
+            <Card className="cv-gauge-temp">
+              <CardHeader title="Temperature" subheader={gauge?.timestamp} />
+              <Gauge data={gauge} />
+            </Card>
+            <Card className="cv-motorcycle-count">
+              <CardHeader title="Motorcycle Count" subheader={count?.timestamp} />
+              <CardContent>{count?.count}</CardContent>
+            </Card>
+          </div>
+          <div id="gauge-right">
+            <Card className="cv-gauge-humidity">
+              <CardHeader title="Humidity" subheader={gauge?.timestamp} />
+              <RadialGauge data={gauge} />
+            </Card>
+          </div>
         </div>
         <div id="row-2">
           <Card className="cv-video-stream">
             <CardContent>
-              <ReactPlayer url='http://192.168.1.88:8888/proxied' playing={true} />
+              <iframe src="http://192.168.1.88:8888/cam1" />
             </CardContent>
           </Card>
         </div>
